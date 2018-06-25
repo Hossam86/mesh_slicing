a3b478b6c47e42a15d7eaf2874af40d5e0148aed
Hossam Samir <eng.hsamir@gmail.com>
2018-06-25 13:43:35 +0200

    implmentaion of plane object

--- Plane.cpp
+++ Plane.cpp
@@ -1,1 +1,17 @@
 #include "Plane.h"
+Plane::Plane(double d, const v3 & normal)
+{
+	this->mDistance;
+	this->normal=normal;
+}
+
+Plane::~Plane()
+{
+
+}
+
+double Plane::distanceToPoint(v3 & point)
+{
+	double result=point.dot(normal);
+	return result;
+}
--- Plane.h
+++ Plane.h
@@ -1,7 +1,13 @@
+#include "v3.h"
 class Plane
 {
 public:
-	Plane();
+	Plane(double distance,const v3 & normal);
 	~Plane();
-	
+	double distanceToPoint(v3 & point);
+
+private:
+	double mDistance;
+	v3 normal;
+
 };
