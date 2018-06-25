80e381f8daa60acd61041548a08ed8ccf7e70fd4
Hossam Samir <eng.hsamir@gmail.com>
2018-06-25 13:44:21 +0200

    implmentaion of triangle object

--- triangle.cpp
+++ triangle.cpp
@@ -1,1 +1,23 @@
 #include "triangle.h"
+
+triangle::triangle(const v3 & p0, const v3 & p1, const v3 & p2)
+{
+p[0]=p0;
+p[1]=p1;
+p[2]=p2;
+}
+
+triangle::~triangle()
+{
+
+}
+
+void triangle::setNormal(const v3 & n)
+{
+	this->normal= n;
+}
+
+v3& triangle:: getNormal()
+{
+ return this->normal;	
+}
--- triangle.h
+++ triangle.h
@@ -1,7 +1,12 @@
+#include "v3.h"
 class triangle
 {
 public:
-	triangle();
+	triangle(const v3 & p0, const v3 & p1, const v3 & p2);
 	~triangle();
-	
+	v3& getNormal();
+	void setNormal(const v3 &N);
+private:
+	v3 p[3];
+    v3 normal;
 };
