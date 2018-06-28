d5eebf2a4577c78dc1a197fa59c96ff2cf830bf8
Hossam Samir <eng.hsamir@gmail.com>
2018-06-28 14:06:03 +0200

    getter and setter function for mesh object

--- triangleMesh.cpp
+++ triangleMesh.cpp
@@ -23,5 +23,45 @@ void triangleMesh::normalize()
     triangle & t =mesh[i];
 	t-= start;
 	}
-
+	bottomLeftVertex=halfbox*-1.0f;
+	topRightVertex=halfbox;
 }
+void triangleMesh::push_back(const triangle & t)
+{
+	mesh.push_back(t);
+}
+
+v3 triangleMesh::meshAABBSize()const
+{
+ return v3(topRightVertex.getX()-bottomLeftVertex.getX(),topRightVertex.getY()-bottomLeftVertex.getY(),topRightVertex.getZ()-bottomLeftVertex.getZ());
+}
+
+std::vector<triangle> & triangleMesh::getMesh()
+{
+	return this->mesh;
+}
+
+const std::vector<triangle> & triangleMesh::getMesh() const 
+{
+	return this->mesh;
+}
+
+v3 triangleMesh::getBottomLeftVertex() const
+{
+	return bottomLeftVertex;
+}
+
+v3 triangleMesh::getTopRightVertex() const
+{
+	return topRightVertex;
+}
+
+// int triangleMesh::transform(const lm:: mat4 &mat)
+// {
+// 	for (size_t i = 0; i < mesh.size(); ++i)
+// 	{
+// 		triangle & tri=mesh[i];
+// 		tri.transform(mat);
+// 	}
+// 	return 0;
+// }
--- triangleMesh.h
+++ triangleMesh.h
@@ -10,6 +10,13 @@ public:
 	~triangleMesh();
 	size_t size();
 	void normalize();
+	void push_back(const triangle & tri);
+	v3 meshAABBSize()const;
+	//int transform(const glm::mat4 & mat) // Mesh COG point should be at (0,0,0)
+	std::vector<triangle> & getMesh();
+	const std::vector<triangle> & getMesh() const;
+	v3 getBottomLeftVertex() const;
+	v3 getTopRightVertex() const;
 
 protected:
 v3 bottomLeftVertex ;
